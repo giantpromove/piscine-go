68aa74dceae35b4c4829c10556e7de6abdc938ba
giantpromove <giantpromove@github.com>
2019-10-14 20:52:31 +0600

    my family

---
+++ myfamily.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
