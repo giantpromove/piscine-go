05cae763249a01908a831cfe30ee7e4fbd371bca
giantpromove <giantpromove@github.com>
2019-10-19 12:27:09 +0600

    commit first

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,8 +1,9 @@
-package piscine
-
-func  UltimatePointOne(n* int) {
-
-	a* = 1
-	b* =&a
-	n* =&b
-}
+package main
+#!/bin/bash
+read -p "Enter number : " n
+if test $n -ge 0
+then
+	echo "$n is positive number."
+else
+	echo "$n number is negative number."
+fi
