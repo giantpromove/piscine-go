c70770f1fdcd4aee989bf35f21e10c724b9649f6
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:33:53 +0600

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,18 +1,17 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	
 	TextToString := ""
 	t := []string{}
 	for i, v := range str {
 		if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
 			TextToString += string(v)
 			t = append(t, TextToString)
-		}else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-				TextToString += string(v)
-		}else{
-			if len(TextToString)>=1{
-				t = append(t, TextToString)
+		} else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+		} else {
+			if len(TextToString) >= 1 {
+ 				t = append(t, TextToString)
 			}
 			TextToString = ""
 		}
