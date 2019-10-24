93d5d843dcdbe4d979c6739c24ccb72d0383f9ea
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:54:13 +0600

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,27 +1,50 @@
 package piscine
 
+func lent(d string) int {
+	inc := 0
+	for range d {
+		inc++
+	}
+	return inc
+}
+
 func SplitWhiteSpaces(str string) []string {
 	TextToString := ""
-	t := []string{}
-	for i, v := range str {
-		if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			TextToString += string(v)
-			t = append(t, TextToString)
-		} else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			TextToString += string(v)
+	startvalue := false
+	count_M := 0
+
+	for ii, v := range str {
+		if string(v) == " " || string(v) == "\t" || string(v) == "\n" {
+			if startvalue == true {
+				count_M++
+				startvalue = false
+			}
 		} else {
-			if len(TextToString) >= 1 {
-				t = append(t, TextToString)
+			if startvalue == false {
+				startvalue = true
+			}
+		}
+		if ii == lent(str)-1 && startvalue == true {
+			count_M++
+
+		}
+	}
+	t := make([]string, count_M)
+	k := 0
+	for i, v := range str {
+		if i == lent(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+			t[k] = TextToString
+		} else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString = TextToString + string(v)
+		} else {
+			if lent(TextToString) >= 1 {
+				t[k] = TextToString
+				k = k + 1
 			}
 			TextToString = ""
 		}
+
 	}
 	return t
-}
-func lent2(d string) int {
-	hway := 0
-	for range d {
-		hway++
-	}
-	return hway
-}
+	
