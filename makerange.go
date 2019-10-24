ffbd327a1cdc262e4633c143f5a425b8b837bc2d
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:10:31 +0600

    Update and rename appendrange.go to makerange.go

---
+++ makerange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+
+	if min >= max{
+		return nil
+	}
+	answer:= make([]int, max- min)
+	for i := range answer{
+		answer[i] = min + i
+	}
+	return answer
+}
