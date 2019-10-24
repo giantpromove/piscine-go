dbb41e3aaa51f521b724cf36884e7dfef969bed7
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:11:31 +0600

    Update makerange.go

--- makerange.go
+++ makerange.go
@@ -2,11 +2,11 @@ package piscine
 
 func MakeRange(min, max int) []int {
 
-	if min >= max{
+	if min >= max {
 		return nil
 	}
-	answer:= make([]int, max- min)
-	for i := range answer{
+	answer := make([]int, max- min)
+	for i := range answer {
 		answer[i] = min + i
 	}
 	return answer
