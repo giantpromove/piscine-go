379fc6afc7ec5a54f973762e7cd6f14c74dc9e67
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:32:41 +0600

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,12 +1,28 @@
-package main
+package piscine
 
-import (
-	"fmt"
-
-	piscine ".."
-)
-
-func main() {
-	str := "Hello how are you?"
-	fmt.Println(piscine.SplitWhiteSpaces(str))
+func SplitWhiteSpaces(str string) []string {
+	
+	TextToString := ""
+	t := []string{}
+	for i, v := range str {
+		if i == lent2(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+			TextToString += string(v)
+			t = append(t, TextToString)
+		}else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
+				TextToString += string(v)
+		}else{
+			if len(TextToString)>=1{
+				t = append(t, TextToString)
+			}
+			TextToString = ""
+		}
+	}
+	return t
 }
+func lent2(d string ) int{
+	hway:=0
+		for _, _= range d{
+			hway ++
+		}
+		 return hway
+	}
