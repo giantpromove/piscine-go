c55674465d28a8937ce7e89271b397dc7f6c9151
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-24 19:18:07 +0600

    Update and rename makerange.go to concatparams.go

---
+++ concatparams.go
@@ -0,0 +1,16 @@
+package main
+
+func ConcatParams(args []string) string {
+	result := ""
+	counter := 0
+	for range args {
+		counter++
+	}
+	for i := 0; i < counter; i++ {
+		result += args[i]
+		if i != counter-1 {
+			result = result + "\n"
+		}
+	}
+	return result
+}
