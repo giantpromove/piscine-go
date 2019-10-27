f8bd3f33eed840119c622e19c2e973c025dd0260
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-28 00:56:54 +0600

    Update main.go

--- boolean/main.go
+++ boolean/main.go
@@ -7,39 +7,37 @@ import (
 
 func printStr(str string) {
 	arrayStr := []rune(str)
-
-	for _, s := range arrayStr {
-		z01.PrintRune(s)
+	length := 0
+	for i := range arrayStr {
+		length = i + 1
+	}
+	for i := 0; i < length; i++ {
+		z01.PrintRune(arrayStr[i])
 	}
 	z01.PrintRune('\n')
 }
 
-func even(nbr int) bool {
-
-	if nbr%2 == 0 {
-		return true
-	}
-	return false
-}
-
 func isEven(nbr int) bool {
-
-	if even(nbr) == true {
+	if nbr%2 != 1 {
 		return true
+	} else {
+		return false
 	}
-	return false
 }
 
 func main() {
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	args := os.Args
+	args = args[1:]
+	lengthOfArg := 0
+	for i := range args {
+		lengthOfArg = i + 1
+	}
 
-	EvenMsg := "I have an even number of arguments\n"
-	OddMsg := "I have an odd number of arguments\n"
-
-	lenOfArg := piscine.Lent3(os.Args) - 1
-
-	if isEven(lenOfArg) {
-		piscine.PrintStr(EvenMsg)
+	if isEven(lengthOfArg) == true {
+		printStr(EvenMsg)
 	} else {
-		piscine.PrintStr(OddMsg)
+		printStr(OddMsg)
 	}
 }
