321b24ddf14ac9023cb838dccfc96f256fa2ed35
giantpromove <54464781+giantpromove@users.noreply.github.com>
2019-10-28 01:03:51 +0600

    Update main.go

--- point/main.go
+++ point/main.go
@@ -1,43 +1,22 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
-	"os"
+	"fmt"
 )
-
-func printStr(str string) {
-	arrayStr := []rune(str)
-	length := 0
-	for i := range arrayStr {
-		length = i + 1
-	}
-	for i := 0; i < length; i++ {
-		z01.PrintRune(arrayStr[i])
-	}
-	z01.PrintRune('\n')
+type point struct {
+  x int
+  y int
 }
 
-func isEven(nbr int) bool {
-	if nbr%2 != 1 {
-		return true
-	} else {
-		return false
-	}
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
 func main() {
-	EvenMsg := "I have an even number of arguments"
-	OddMsg := "I have an odd number of arguments"
-	args := os.Args
-	args = args[1:]
-	lengthOfArg := 0
-	for i := range args {
-		lengthOfArg = i + 1
-	}
+	points := &point{}
 
-	if isEven(lengthOfArg) == true {
-		printStr(EvenMsg)
-	} else {
-		printStr(OddMsg)
-	}
+	setPoint(points)
+
+	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
 }
